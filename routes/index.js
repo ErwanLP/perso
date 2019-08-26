3c2b18ef10b85119778415c2ad84c5f35729dcc0
erwan <erwan.le.poder@gmail.com>
2019-08-26 22:56:16 +0200

    V2

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Erwan Le Poder' });
 });
 
 module.exports = router;

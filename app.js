57e7ff08473d0f096c7a177cf04b0619501bfe99
erwan <erwan.le.poder@gmail.com>
2019-08-28 21:47:23 +0200

    opti + image

--- app.js
+++ app.js
@@ -1,22 +1,24 @@
 var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+//var cookieParser = require('cookie-parser');
+//var logger = require('morgan');
+var compression = require('compression')
 
 var indexRouter = require('./routes/index');
 var filesRouter = require('./routes/files');
 
 var app = express();
+app.use(compression())
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
-app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
+//app.use(logger('dev'));
+//app.use(express.json());
+//app.use(express.urlencoded({ extended: false }));
+//app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
